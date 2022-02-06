2e308ffa8fff04ca384b01ada6503902f6d7dc29
marckesin <192.168.2.1@protonmail.com>
2022-02-05 22:28:31 -0300

    implementação da verificação de formulário vazio

---
+++ javascripts/script.js
@@ -0,0 +1,41 @@
+// Função aplica o estilo nos inputs de acordo com seu valor
+// Inputs sem valor mostram alerta na tela
+function changeStyle(list, color, display) {
+  list.forEach(element => {
+    const id = element[0];
+    const input = document.getElementById(id);
+    const iconError = input.nextElementSibling;
+    const warningMessage = iconError.nextElementSibling;
+
+    input.style.borderColor = color;
+    iconError.style.display = display;
+    warningMessage.style.display = display;
+  });
+}
+
+// Função verifica o valor recebido de cada input e chama a função
+// changeStyle para mostrar ou não algum alerta
+function emptyInputs(obj) {
+  if (!obj.firstName || !obj.lastName || !obj.email || !obj.password) {
+    const emptyInputs = Object.entries(obj).filter(item => !item[1]);
+    const notEmptyInputs = Object.entries(obj).filter(item => item[1]);
+
+    changeStyle(emptyInputs, "hsl(0, 100%, 74%)", "block");
+    changeStyle(notEmptyInputs, "hsl(0, 0%, 83%)", "none");
+
+    return 0;
+  }
+  changeStyle(Object.entries(obj), "hsl(0, 0%, 83%)", "none");
+}
+
+document.getElementById("submit").addEventListener("click", event => {
+  const items = {
+    firstName: document.getElementById("firstName").value,
+    lastName: document.getElementById("lastName").value,
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value,
+  };
+
+  event.preventDefault();
+  emptyInputs(items);
+});
